de2f72beed6c927ecf3bca177a3ad696317d6296
bogdanbaciu <vlabac.alu.edu.gva@es>
2026-01-20 02:16:06 +0100

    Trabajando en la seccion biográfica

---
+++ main.js
@@ -0,0 +1,24 @@
+// Initialize Lenis smooth scroll
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smooth: true,
+  smoothTouch: false,
+});
+
+// Animation frame loop
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+// Optional: Sync Lenis with GSAP ScrollTrigger
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
